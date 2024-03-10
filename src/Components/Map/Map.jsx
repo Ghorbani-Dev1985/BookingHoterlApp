e6d1b1624cdb31dd05ef189988210b6ac1151762
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-03-10 13:41:39 +0330

    feat: develop add map in map component and show hotels in it

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHotels } from '../Context/HotelsProvider'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const Map = () => {
 const {isLoading , hotels} = useHotels()
+const [mapCenter , setMapCenter] = useState([51 , 3])
   return (
-    <section className='overflow-y-scroll h-full'>Map</section>
+    <section className='overflow-y-scroll h-full min-h-screen'>
+         <MapContainer className='h-full' center={mapCenter} zoom={13} scrollWheelZoom={true}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+    />
+   {
+    hotels.map(({id , latitude , longitude , host_location})=> {
+        return(
+            <React.Fragment key={id}>
+                 <Marker position={[latitude , longitude]}>
+      <Popup>
+       {host_location}
+      </Popup>
+    </Marker>
+            </React.Fragment>
+        )
+    })
+   }
+  </MapContainer>
+    </section>
   )
 }
 
